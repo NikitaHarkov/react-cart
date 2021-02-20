de6ad2a237ec46da1b430352eb31a8fe8c78ca97
«Nikita <mrnikita.harkov@gmail.com>
2021-02-20 13:43:34 +0200

    Refactor increase/decrease functionality

--- src/reducer.js
+++ src/reducer.js
@@ -37,21 +37,16 @@ const reducer = (state, action) => {
     return { ...state, cart: newCart };
   }
 
-  if (action.type === 'INCREASE') {
-    let tempCart = state.cart.map(cartItem => {
-      if (cartItem.id === action.payload) {
-        return { ...cartItem, amount: cartItem.amount + 1 };
-      }
-      return cartItem;
-    });
-    return { ...state, cart: tempCart };
-  }
-
-  if (action.type === 'DECREASE') {
+  if (action.type === 'TOGGLE_AMOUNT') {
     let tempCart = state.cart
       .map(cartItem => {
-        if (cartItem.id === action.payload) {
-          return { ...cartItem, amount: cartItem.amount - 1 };
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === 'inc') {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+          if (action.payload.type === 'dec') {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
         }
         return cartItem;
       })
@@ -59,7 +54,7 @@ const reducer = (state, action) => {
     return { ...state, cart: tempCart };
   }
 
-  return state;
+  throw new Error(`No matching action type: ${action.type}`);
 };
 
 export default reducer;
